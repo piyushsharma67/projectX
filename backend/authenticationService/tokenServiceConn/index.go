9b64f95ddf00cb59dae59a5713569c0f13371b6f
piyush672 <piyush@farmart.co>
2024-03-25 05:00:01 +0530

    added

--- backend/authenticationService/tokenServiceConn/index.go
+++ backend/authenticationService/tokenServiceConn/index.go
@@ -13,13 +13,9 @@ type TokenServiceConn struct{
 	TokenClient tokenRpcServerProtoFiles.TokenServiceClient
 }
 
-const (
-    tokenServiceAddr = "tokenservice" // Assuming token service listens on port 5001
-)
-
 func New()(*TokenServiceConn,error){
 
-	grpcPortTarget:=fmt.Sprintf("tokenservice:%s",os.Getenv("grpcPort"))
+	grpcPortTarget:=fmt.Sprintf("%s:%s",os.Getenv("tokenservice"),os.Getenv("grpcPort"))
 	
 	conn,err:=grpc.Dial(grpcPortTarget,grpc.WithInsecure())
 	
@@ -33,12 +29,6 @@ func New()(*TokenServiceConn,error){
 	tsc:=&TokenServiceConn{}
 
 	tsc.TokenClient=tokenServiceClientConn
-
-	log.Printf("Connected to token service at %s", tokenServiceAddr)
-
-	if(err!=nil){
-		fmt.Println("nmnn"+err.Error())
-	}
 
 	return tsc,nil
 }
